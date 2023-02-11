f8a7e83d0ee8f9f1cdb219df8d5fcd57a880f9a8
ginojohnson0110 <ginojohnson0110@gmail.com>
2023-02-11 10:31:46 +0530

    admno

--- Registration/src/main/java/com/example/demo/Registrationcontroller.java
+++ Registration/src/main/java/com/example/demo/Registrationcontroller.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,4 +27,10 @@ public class Registrationcontroller {
 	{
 	    return repo.findAll(); 
 	}
+
+
+@DeleteMapping("/del/{uid}")
+public void deleteEmployee(@PathVariable String uid) {
+   repo.deleteById(uid);
+}
 }
